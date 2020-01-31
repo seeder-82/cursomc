80c8e3c4f4bda42b13c01cb563e8212240761255
Claudio <claudio.silva0@gmail.com>
2020-01-31 16:44:20 +0000

    Projecto criado

---
+++ src/test/java/com/seeder/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeder.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
