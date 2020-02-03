8b3e0e7abb463edb70d2c256d4665576c8017f68
claudio <claudio.silva@euroralex.pt>
2020-02-03 20:44:39 +0000

    Estado e cidade

--- src/main/java/com/seeder/cursomc/CursomcApplication.java
+++ src/main/java/com/seeder/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.seeder.cursomc;
 
 import com.seeder.cursomc.domain.Categoria;
+import com.seeder.cursomc.domain.Cidade;
+import com.seeder.cursomc.domain.Estado;
 import com.seeder.cursomc.domain.Produto;
 import com.seeder.cursomc.repositories.CategoriaRepository;
+import com.seeder.cursomc.repositories.CidadeRepository;
+import com.seeder.cursomc.repositories.EstadoRepository;
 import com.seeder.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -18,6 +22,10 @@ public class CursomcApplication implements CommandLineRunner {
     private CategoriaRepository categoriaRepository;
     @Autowired
     private ProdutoRepository produtoRepository;
+    @Autowired
+    private EstadoRepository estadoRepository;
+    @Autowired
+    private CidadeRepository cidadeRepository;
 
     public static void main(String[] args) {
 
@@ -42,6 +50,22 @@ public class CursomcApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
         produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null,"Uberlandia", est1);
+        Cidade c2 = new Cidade(null,"São Paulo", est2);
+        Cidade c3 = new Cidade(null,"Campinas", est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2,c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1,est2));
+        cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+
+
+
 
     }
 }
---
+++ src/main/java/com/seeder/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.seeder.cursomc.repositories;
+
+import com.seeder.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
