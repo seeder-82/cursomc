c33117bd7e1b35a96c85082e2f0250b54b8f1fac
Claudio <claudio.silva0@gmail.com>
2020-02-05 10:01:31 +0000

    Pedido, Estado de pagamento, pagamento

---
+++ src/main/java/com/seeder/cursomc/enums/EstadoPagamento.java
@@ -0,0 +1,44 @@
+package com.seeder.cursomc.enums;
+
+public enum EstadoPagamento {
+
+    PENDENTE(1, "Pendente"),
+    QUITADO(2,"Quitado"),
+    CANCELADO(3, "Cancelado");
+
+    private int cod;
+    private String decricao;
+
+    EstadoPagamento(int cod, String decricao) {
+        this.cod = cod;
+        this.decricao = decricao;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getDecricao() {
+        return decricao;
+    }
+
+    public void setDecricao(String decricao) {
+        this.decricao = decricao;
+    }
+
+    public static EstadoPagamento toEnum(Integer cod){
+        if (cod == null){
+            return null;
+        }
+        for (EstadoPagamento x : EstadoPagamento.values()){
+            if (cod.equals(x.getCod())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Id invalido: "+ cod);
+    }
+}
